9f74e8f62521b08d4c9fb690ae9c9b54a59889ce
abdulhakeembsk4k59 <sk.a.hakeem123@gmail.com>
2023-07-26 00:34:29 +0530

    login update

---
+++ js/login_src.js
@@ -0,0 +1,10 @@
+function validate(form) {
+    let userName = form.username.value;
+    const password = form.password.value;
+    // console.log(userName, password);
+    if (userName == "admin@admin.com" && password == "123456") {
+        window.location.href = "../../pages/regular_pages/success.html";
+    }else{
+
+    }
+}
