b4d67c185075dc4599a14eac3cd36a4b6ea71fbc
abdulhakeembsk4k59 <sk.a.hakeem123@gmail.com>
2023-08-06 10:43:55 +0530

    Final Commit

--- js/login_src.js
+++ js/login_src.js
@@ -2,8 +2,8 @@ function validate(form) {
     let userName = form.username.value;
     const password = form.password.value;
     // console.log(userName, password);
-    if (userName == "admin@admin.com" && password == "123456") {
-        window.location.href = "../../pages/regular_pages/success.html";
+    if (userName === "admin@admin.com" && password === "123456") {
+        alert("Login successfull")
     }else{
         alert("Email or password is incorrect!");
     }
